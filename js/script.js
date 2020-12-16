2e1146b9e33cad5fc998368caf6758ab69e0cd6a
Simone Romiti <sr1992ei@gmail.com>
2020-12-16 15:41:34 +0100

    Stampa 10 random email con axios

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var app = new Vue(
+  {
+    el: "#container",
+    data: {
+      emailList: []
+    },
+
+    mounted: function(){
+      const self = this;
+
+      for(var i = 0; i < 10; i++){
+        axios
+        .get(' https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(function (response) {
+          self.emailList.push(response.data.response);
+        });
+
+      }
+
+    },
+
+    methods: {
+
+    }
+  }
+);
