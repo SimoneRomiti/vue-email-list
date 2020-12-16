bd62df46b37ec949a1441a394b1b68519a97e5c6
Simone Romiti <sr1992ei@gmail.com>
2020-12-16 18:47:10 +0100

    Funzione per colori casuali su li, e v-if su ul appena tutti pronti

--- js/script.js
+++ js/script.js
@@ -2,25 +2,39 @@ var app = new Vue(
   {
     el: "#container",
     data: {
-      emailList: []
+      emailList: [],
+      colorArray: [],
+      color: "#",
+      letters: '0123456789ABCDEF',
+    },
+
+    created: function(){
+      for(var i = 0; i < 10; i++){
+        this.colorArray.push(this.randomColor())
+      }
+      console.log(this.colorArray);
     },
 
     mounted: function(){
       const self = this;
-
       for(var i = 0; i < 10; i++){
         axios
         .get(' https://flynn.boolean.careers/exercises/api/random/mail')
         .then(function (response) {
           self.emailList.push(response.data.response);
         });
-
       }
-
     },
 
     methods: {
+      randomColor: function(){
+        this.color = "#";
+        for(var i = 0; i < 6; i++){
+          this.color += this.letters[Math.floor(Math.random() * 16)]
+        }
+        return this.color;
 
+      }
     }
   }
 );
